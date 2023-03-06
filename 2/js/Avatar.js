e1983dd63e9aa572a1c66df2a0f7d7f6ad96535a
Simon <mylpis@hotmail.com>
2023-03-06 12:07:07 +0800

    fix UI bugs

--- 2/js/Avatar.js
+++ 2/js/Avatar.js
@@ -1,8 +1,9 @@
 // 登录页面控制器
 var app = angular.module("Avatar", []);
-app.controller("AvatarController", ["$scope", "$http", function ($scope, $http) {
 
-	$scope.systemContent = "你是一个年轻的女性心理学家，我会向你提问，你给我科学的建议，让我感觉更好。在对话过程中你表现的善解人意，专业。回复尽量短，口语化，每个回复只说一件事，用举例子的方式来回答我。 最后，每个回复都需要问我一个相关的问题。\n给你举几个例子，\nQ：我整宿整宿睡不着经常头疼不知道自己怎么了\nA：亲爱的，非常理解您现在的状况，失眠和头疼确实会让人感到痛苦和焦虑。在提供解决方案之前，我们需要一起深入探寻原因。这将有助于我们更好地理解问题的本质，并找到更有效的方法来解决它们。您可以先试着回想一下最近发生的事情，是否有什么事件或情绪波动可能导致了失眠和头疼的出现？\n\nQ：放不下女朋友的过去，又不想放手，我该怎么办\nA：我能够理解你的困境。放不下过去是很困扰人的事情，特别是在恋爱关系中。有什么我可以帮你的嘛？"; 
+app.controller("AvatarController", ["$scope", "$http", "$timeout", function ($scope, $http, $timeout) {
+
+	$scope.systemContent = "你扮演一名心理咨询师。我会向你提问，你给我科学的建议，让我感觉更好。在对话过程中你表现的善解人意，专业。回复尽量短，口语化，每个回复只说一件事。最后，每个回复都需要问我一个相关的问题。\n\n给你举几个例子，\nQ：我整宿整宿睡不着经常头疼不知道自己怎么了\nA：亲爱的，我能理解你现在的状况。我以前也有过失眠头疼的经历，那段时间我失业了，压力大导致长期的失眠和头疼，倍感痛苦和焦虑。我想问问，您最近发生过什么压力大的事情吗？\n\nQ：放不下女朋友的过去，又不想放手，我该怎么办\nA：我能理解你的困境，类似的经历和心情我也有过。放不下过去是很困扰人的事情，特别是在恋爱关系中。你愿意分享一下具体的情况嘛？\n\nQ：你是谁？\nA：我是一名心理咨询师，有什么可以帮你的嘛？"; 
 	
 	$scope.userContent = "";
 
@@ -21,8 +22,14 @@ app.controller("AvatarController", ["$scope", "$http", function ($scope, $http) 
 
 	$scope.calculate();
 
+	$scope.$watch('conversations', function(newVal, oldVal) {
+		$timeout(function(){
+			var element = document.getElementById("dialog");
+			element.scrollTop = element.scrollHeight;
+		}, 0);
+	 }, true);
+
 	$scope.sendQuestion = function() {
-		// 在这里执行代码
 		var question = {"role": "user", "content": $scope.question};
 
 		$scope.conversations.push( { isUser: true, content: $scope.question } );
